3a83d634d0b440455f46ca5e63891ae89c7bc635
sebastientromp <sebastien.tromp@gmail.com>
2023-09-15 10:37:44 +0200

    add methods to retrieve patch info

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,8 +1,8 @@
 export * from './services/logger';
+export * from './services/patch-info';
 export * from './services/rds';
 export * from './services/s3';
 export * from './services/security';
 export * from './services/sns';
 export * from './services/sqs';
 export * from './services/utils';
-
---
+++ src/services/patch-info.ts
@@ -0,0 +1,35 @@
+import { http } from './utils';
+
+export const getPatchInfos = async (): Promise<PatchesConfig> => {
+	const patchInfo = await http(`https://static.zerotoheroes.com/hearthstone/data/patches.json`);
+	const structuredPatch = JSON.parse(patchInfo);
+	return structuredPatch;
+};
+
+export const getLastBattlegroundsPatch = async (): Promise<PatchInfo> => {
+	const config = await getPatchInfos();
+	const patchNumber = config.currentBattlegroundsMetaPatch;
+	return config.patches.find(patch => patch.number === patchNumber);
+};
+
+export const getLastConstructedPatch = async (): Promise<PatchInfo> => {
+	const config = await getPatchInfos();
+	const patchNumber = config.currentConstructedMetaPatch;
+	return config.patches.find(patch => patch.number === patchNumber);
+};
+
+export interface PatchesConfig {
+	readonly patches: readonly PatchInfo[];
+	readonly currentConstructedMetaPatch: number;
+	readonly currentBattlegroundsMetaPatch: number;
+	readonly currentDuelsMetaPatch: number;
+	readonly currentArenaMetaPatch: number;
+	readonly currentMercenariesMetaPatch: number;
+}
+
+export interface PatchInfo {
+	readonly number: number;
+	readonly version: string;
+	readonly name: string;
+	readonly date: string;
+}
